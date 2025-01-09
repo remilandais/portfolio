d1f123844a7268d7782133929a39debb615b6821
remilandais <remi.landais@contentsquare.com>
2025-01-09 20:01:58 +0100

    toto

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
         <nav></nav>
       </header>
       <main>
-        <h1 className="big-text">Hello I am Rémi, an incredible artist and developer</h1>
+        <h1 className="big-text">Hello I am Rémi, an incredible artist and dev</h1>
       </main>
       <footer></footer>
     </>
